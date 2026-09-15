f308d10c3eafeaadf860431ec662b5aa211f8f07
Gustavo Lopes <guhrlopes999@gmail.com>
2026-09-15 11:18:13 -0300

    initial spring

---
+++ src/main/java/br/com/desenvolvedorgustavolopes/moneyPilot/MoneyPilotApplication.java
@@ -0,0 +1,13 @@
+package br.com.desenvolvedorgustavolopes.moneyPilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyPilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desenvolvedorgustavolopes/moneyPilot/MoneyPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desenvolvedorgustavolopes.moneyPilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
